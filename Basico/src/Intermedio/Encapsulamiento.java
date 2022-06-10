cfe8ba28e63a80fae44b0c53acbd6fe33f62c2b7
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-06-09 22:57:19 -0300

    Encapsulamiento

---
+++ Basico/src/Intermedio/Encapsulamiento.java
@@ -0,0 +1,11 @@
+package Intermedio;
+
+public class Encapsulamiento {
+
+    int edad;                       //Default
+    public String nombre;           //public
+    protected String apellido;      //protected
+    private long DNI;               //private
+
+    //https://www.monografias.com/trabajos107/introduccion-plataforma-java/Diapositiva58.png
+}
