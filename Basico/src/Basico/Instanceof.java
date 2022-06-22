9708948ca802443a2824e8f758bafac8355a83c5
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-06-22 11:54:59 -0300

    Thows y Instanceof

---
+++ Basico/src/Basico/Instanceof.java
@@ -0,0 +1,9 @@
+package Basico;
+
+public class Instanceof {
+    public static void main(String[] args) {
+        Instanceof myObj = new Instanceof();
+        System.out.println(myObj instanceof Instanceof); // returns true
+    }
+
+}
---
+++ Basico/src/Intermedio/Thows.java
@@ -0,0 +1,19 @@
+package Intermedio;
+
+public class Thows {
+
+}
+
+class Main {
+    static void checkAge(int age) throws ArithmeticException{
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAge(15); // Set age to 15 (which is below 18...)
+    }
+}
